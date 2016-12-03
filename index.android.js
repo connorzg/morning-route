cbcb8f04a1c072af174360bddb72d76404544444
Connor Garber <connor.garber@gmail.com>
2016-12-02 22:09:39 -0600

    handle input and query string

--- index.android.js
+++ index.android.js
@@ -17,7 +17,36 @@ export default class MorningRoute extends Component {
             summary: ''
         };
     }
-    
+
+    _setStart(text) {
+        console.log(this.state.endText);
+        this.setState({startText: text});
+    }
+
+    _setEnd(text) {
+        this.setState({endText: text});
+        console.log(this.state.endText);
+    }
+
+    _getRoute(start, end) {
+        let query = `https://maps.googleapis.com/maps/api/directions/json?origin=${start}&destination=${end}&key=AIzaSyB3xsLMFn2XoZfmywOnsWn8tf0Ffvw7FF0`
+        fetch(query).then((response) => response.json()).then((responseText) => {
+            this.setState({summary: responseText});
+            console.log(this.state.summary);
+        }).catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    _handleInput() {
+        console.log(this.state.startText, this.state.endText);
+        var locations = [this.state.startText, this.state.endText];
+        locations.forEach(function(location) {
+            location.replace(/\s+/g, '+');
+        })
+        _getRoute(locations[0], locations[1]);
+    }
+
     render() {
         return (
             <View style={styles.container}>
